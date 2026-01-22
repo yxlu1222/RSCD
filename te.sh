abf3350aaf56dbe87309706ea737f8d94bcdf316
yxlu1222 <441744591@qq.com>
2026-01-22 09:41:10 +0800

    Fix: Optimize InfoNCE logic and weight

--- te.sh
+++ te.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # python tools/copy_clip_files.py LEVIRCD penalty_10_mask
-bash tools/general/test.sh LEVIR configs/MdaCD_LEVIRCD.py 1 /home/dell/gitrepos/MdaCD/work_dirs/MdaCD_LEVIRCD_3
+bash tools/general/test.sh LEVIR configs/MdaCD_LEVIRCD.py 1 /home/dell/gitrepos/MdaCD/work_dirs/MdaCD_LEVIRCD_4
 
 # python tools/copy_clip_files.py SYSUCD penalty_100_mask
-bash tools/general/test.sh SYSU configs/MdaCD_SYSUCD.py 1 /home/dell/gitrepos/MdaCD/work_dirs/MdaCD_SYSUCD_3
+bash tools/general/test.sh SYSU configs/MdaCD_SYSUCD.py 1 /home/dell/gitrepos/MdaCD/work_dirs/MdaCD_SYSUCD_4
 
 # 举例：生成可视化结果（自动查找权重文件）
-CHECKPOINT=$(find work_dirs/MdaCD_LEVIRCD_3 -name 'best_mIoU_iter_*.pth' -type f | head -n 1)
+CHECKPOINT=$(find work_dirs/MdaCD_LEVIRCD_4 -name 'best_mIoU_iter_*.pth' -type f | head -n 1)
 if [ -f "$CHECKPOINT" ]; then
     bash tools/general/dist_test.sh configs/MdaCD_LEVIRCD.py \
         "$CHECKPOINT" \
         1 \
-        --show-dir work_dirs/MdaCD_LEVIRCD_3/vis_result
+        --show-dir work_dirs/MdaCD_LEVIRCD_4/vis_result
 else
-    echo "Error: No checkpoint found in work_dirs/MdaCD_LEVIRCD_3"
+    echo "Error: No checkpoint found in work_dirs/MdaCD_LEVIRCD_4"
 fi 
--- tr.sh
+++ tr.sh
@@ -4,8 +4,8 @@
 # rm -rf /home/dell/gitrepos/MdaCD/work_dirs
 
 python tools/copy_clip_files.py LEVIRCD penalty_10_mask
-bash tools/general/train.sh configs/MdaCD_LEVIRCD.py 1 --work-dir /home/dell/gitrepos/MdaCD/work_dirs/MdaCD_LEVIRCD_3
+bash tools/general/train.sh configs/MdaCD_LEVIRCD.py 1 --work-dir /home/dell/gitrepos/MdaCD/work_dirs/MdaCD_LEVIRCD_4
 
 python tools/copy_clip_files.py SYSUCD penalty_100_mask
-bash tools/general/train.sh configs/MdaCD_SYSUCD.py 1 --work-dir /home/dell/gitrepos/MdaCD/work_dirs/MdaCD_SYSUCD_3
+bash tools/general/train.sh configs/MdaCD_SYSUCD.py 1 --work-dir /home/dell/gitrepos/MdaCD/work_dirs/MdaCD_SYSUCD_4
 
